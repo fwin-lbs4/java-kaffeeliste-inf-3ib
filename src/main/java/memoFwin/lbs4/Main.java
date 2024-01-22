7409858f12057b26edef9eff2e97e99e9fec3a06
fwin-lb4 <florian.windisch@lbs4.salzburg.at>
2024-01-22 10:09:26 +0100

    Hallo Memo

--- src/main/java/memoFwin/lbs4/Main.java
+++ src/main/java/memoFwin/lbs4/Main.java
@@ -3,6 +3,7 @@ package memoFwin.lbs4;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hallo Flo");
+        System.out.println("Hallo Memo");
         System.out.println("Hello world!");
     }
 }
