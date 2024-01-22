c9ff8328684bc99ab1f26d4ed9954c232509afbb
MemoLBS4 <Mehmet.babat@lbs4.salzburg.at>
2024-01-22 10:08:26 +0100

    Hallo Flo

--- src/main/java/memoFwin/lbs4/Main.java
+++ src/main/java/memoFwin/lbs4/Main.java
@@ -2,6 +2,7 @@ package memoFwin.lbs4;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hallo Flo");
         System.out.println("Hello world!");
     }
 }
