bcc7ad03fe04e83cc53f13647178030a81473e86
fwin-lb4 <florian.windisch@lbs4.salzburg.at>
2024-01-29 10:26:36 +0100

    Add coffelist to view

--- src/main/java/memoFwinAdrian/lbs/vier/SelectCoffeeController.java
+++ src/main/java/memoFwinAdrian/lbs/vier/SelectCoffeeController.java
@@ -1,20 +1,73 @@
 package memoFwinAdrian.lbs.vier;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SelectCoffeeController extends GenericController {
+    private final List<Coffee> coffeeList = new ArrayList<>();
+    private Coffee selectedCoffee;
     @FXML
-    private Label welcomeText;
+    private VBox coffeeBox;
+    @FXML
+    private Button submitButton;
+    @FXML
+    protected void onRefreshButtonClick() {
+        if (this.currentUser == null) {
+            this.onBackButtonClick();
+        }
+        this.submitButton.setDisable(this.selectedCoffee == null);
+        ObservableList<Node> children = this.coffeeBox.getChildren();
+        children.clear();
+        this.coffeeList.clear();
+        this.coffeeList.addAll(this.db.getAllCoffees());
+
+        this.coffeeList.forEach((coffee) -> {
+            Button coffeeButton = new Button();
+            coffeeButton.setText(coffee.toString());
+            coffeeButton.setMaxWidth(Double.POSITIVE_INFINITY);
+            this.setButton(
+                    coffeeButton,
+                    this.selectedCoffee != null && this.selectedCoffee.getIdKaffee() == coffee.getIdKaffee()
+            );
+            coffeeButton.setOnAction((button) -> {
+                this.selectedCoffee = coffee;
+                this.onRefreshButtonClick();
+            });
+            children.add(coffeeButton);
+        });
+    }
+
+    protected void setButton(Button button, boolean active) {
+        button.setStyle(active ? """
+                    -fx-background-color: rgb(0,255,0,0.25);
+                    -fx-border-color: rgb(0, 255, 255);
+                """ : "");
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Select Coffee!");
+    protected void onSubmitButtonClick() {
+        if (this.currentUser != null && this.selectedCoffee != null) {
+            this.currentUser.setSchulden(this.selectedCoffee.getPreis());
+        }
+        this.onBackButtonClick();
     }
 
     @FXML
     protected void onBackButtonClick() {
+        this.selectedCoffee = null;
+        this.currentUser = null;
         this.stage.setTitle("Select User!");
         this.scene.setRoot(this.previousRoot);
+    }
+
+    @Override
+    public void init() {
+        this.onRefreshButtonClick();
     }
 }
