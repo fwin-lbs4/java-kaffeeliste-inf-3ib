d30557981c4fe87478e9b1af59efaa6f5e0cdb53
fwin-lb4 <florian.windisch@lbs4.salzburg.at>
2024-02-01 09:14:19 +0100

    Aktualisiere Schuldenanzeige

--- src/main/java/memoFwinAdrian/lbs/vier/SelectCoffeeController.java
+++ src/main/java/memoFwinAdrian/lbs/vier/SelectCoffeeController.java
@@ -2,10 +2,15 @@ package memoFwinAdrian.lbs.vier;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +24,9 @@ public class SelectCoffeeController extends GenericController {
     @FXML
     private Button adminButton;
     @FXML
-    private Label selectedUserLabel;
+    private Label selectedUserNameLabel;
+    @FXML
+    private Label selectedUserSchuldenLabel;
     private Coffee selectedCoffee;
     @FXML
     private VBox coffeeBox;
@@ -66,9 +73,27 @@ public class SelectCoffeeController extends GenericController {
             this.onBackButtonClick();
         }
 
-        this.selectedUserLabel.setText("User \"" + this.currentUser
-                .getUser()
-                .getName() + "\" Schulden: \"" + this.currentUser.getUser().getSchuldenString() + "\"");
+        this.selectedUserNameLabel.setText("User: " + this.currentUser.getUser().getName());
+
+        this.selectedUserSchuldenLabel.setText(this.currentUser.getUser().getSchuldenString());
+
+        int schulden = this.currentUser.getUser().getSchulden();
+
+        Color bgColor = new Color(0, 0, 0, 0);
+
+        if (schulden < 0) {
+            bgColor = new Color(0, 1, 0, 0.25);
+        }
+
+        if (schulden > 0) {
+            bgColor = new Color(1, 0, 0, 0.25);
+        }
+
+        this.selectedUserSchuldenLabel.setBackground(new Background(new BackgroundFill(
+                bgColor,
+                new CornerRadii(0),
+                new Insets(0)
+        )));
 
         if (Objects.equals(this.currentUser.getUser().getRolle(), "Administrator")) {
             this.adminButton.setVisible(true);
--- src/main/java/memoFwinAdrian/lbs/vier/User.java
+++ src/main/java/memoFwinAdrian/lbs/vier/User.java
@@ -7,44 +7,64 @@ public class User {
     private String rolle;
     private int pin;
 
-    public User(String name, int schulden, int idUser, String rolle, int pin){
+    public User(String name, int schulden, int idUser, String rolle, int pin) {
         this.name = name;
         this.schulden = schulden;
         this.idUser = idUser;
         this.rolle = rolle;
         this.pin = pin;
     }
+
     public int getIdUser() {
         return idUser;
     }
+
     public String getName() {
         return name;
     }
+
     public int getSchulden() {
         return schulden;
-    }
-    public String getRolle() {
-        return rolle;
     }
 
     public void setSchulden(int schulden) {
         this.schulden = schulden;
     }
+
+    public String getRolle() {
+        return rolle;
+    }
+
     public int getPin() {
         return this.pin;
     }
+
     public String toString() {
         return this.getName();
     }
 
     public String getSchuldenString() {
-        String schulden = Integer.toString(this.schulden);
-        if (schulden.length() < 3) {
-            schulden = "0".repeat(3 - schulden.length()) + schulden;
-        }
-        String cents = schulden.substring(schulden.length() - 2);
-        String euro = schulden.substring(0, schulden.length() - 2);
+        int schuldenValue = this.schulden;
 
-        return "€ " + euro + "," + (cents.equals("00") ? "--" : cents);
+        String schuldenType = "";
+        if (schuldenValue >= 0) {
+            schuldenType = "Schulden";
+        } else {
+            schuldenType = "Guthaben";
+            schuldenValue = schuldenValue * -1;
+        }
+
+        String schuldenValueString = Integer.toString(schuldenValue);
+
+        if (schuldenValueString.length() < 3) {
+            schuldenValueString = "0".repeat(3 - schuldenValueString.length()) + schuldenValueString;
+        }
+
+        String cents = schuldenValueString.substring(schuldenValueString.length() - 2);
+        String euro = schuldenValueString.substring(0, schuldenValueString.length() - 2);
+
+        schuldenValueString = "€ " + euro + "," + (cents.equals("00") ? "--" : cents);
+
+        return schuldenType + ": " + schuldenValueString;
     }
 }
