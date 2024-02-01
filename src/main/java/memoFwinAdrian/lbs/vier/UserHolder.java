095cab3b42bf6dde157c63432d85d14371063730
fwin-lbs4 <florian.windisch@lbs4.salzburg.at>
2024-02-01 11:01:49 +0100

    UserHolder add JavaDoc

--- src/main/java/memoFwinAdrian/lbs/vier/UserHolder.java
+++ src/main/java/memoFwinAdrian/lbs/vier/UserHolder.java
@@ -1,19 +1,45 @@
 package memoFwinAdrian.lbs.vier;
 
+/**
+ * Klasse, um den aktuellen User zu halten und mit den Views zu synchronisieren.
+ */
 public final class UserHolder {
-    private User user;
+    /**
+     * Die interne Instanz des Userholders.
+     */
     private final static UserHolder INSTANCE = new UserHolder();
-    private UserHolder() {}
+    /**
+     * Der aktuelle User.
+     */
+    private User user;
 
+    private UserHolder() {
+    }
+
+    /**
+     * Getter für die globale Instanz.
+     *
+     * @return Die globale Instanz des Userholders.
+     */
     public static UserHolder getInstance() {
         return INSTANCE;
     }
 
-    public void setUser(User user) {
-        this.user = user;
-    }
-
+    /**
+     * Getter für den User.
+     *
+     * @return User
+     */
     public User getUser() {
         return this.user;
+    }
+
+    /**
+     * Setter für den User.
+     *
+     * @param user der neue User.
+     */
+    public void setUser(User user) {
+        this.user = user;
     }
 }
