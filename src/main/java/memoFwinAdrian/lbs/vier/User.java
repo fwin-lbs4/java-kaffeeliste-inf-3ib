3ca9eac47975c8237bdc26622624c9c9083b93e8
fwin-lb4 <florian.windisch@lbs4.salzburg.at>
2024-02-01 09:29:01 +0100

    Add JavaDoc for User

--- src/main/java/memoFwinAdrian/lbs/vier/User.java
+++ src/main/java/memoFwinAdrian/lbs/vier/User.java
@@ -1,12 +1,41 @@
 package memoFwinAdrian.lbs.vier;
 
+/**
+ * User-Klasse repräsentiert einen User.
+ * Der User hat einen Namen, eine eindeutige ID, eine Rolle, eventuell einen Pin und Schulden/Guthaben.
+ * Sie stellt diverse Getter/Setter und Hilfs-Methoden bereit.
+ */
 public class User {
+    /**
+     * Name des Users.
+     */
     private final String name;
+    /**
+     * Eindeutige ID des Users.
+     */
     private final int idUser;
+    /**
+     * Die Rolle des Users.
+     */
     private final String rolle;
+    /**
+     * Der Pin des Users.
+     */
     private final int pin;
+    /**
+     * Der Schulden/Guthaben-Stand des Users.
+     */
     private int schulden;
 
+    /**
+     * Konstruiert ein neues User-Objekt mit dem gegebenen Namen, den Schulden, der ID, der Rolle und dem Pin.
+     *
+     * @param name     Name des Users.
+     * @param schulden Schulden/Guthaben des Users.
+     * @param idUser   ID des Users.
+     * @param rolle    Rolle des Users.
+     * @param pin      Pin des Users.
+     */
     public User(String name, int schulden, int idUser, String rolle, int pin) {
         this.name = name;
         this.schulden = schulden;
@@ -15,34 +44,74 @@ public class User {
         this.pin = pin;
     }
 
+    /**
+     * Gibt ID des Users zurück.
+     *
+     * @return ID des Users.
+     */
     public int getIdUser() {
         return idUser;
     }
 
+    /**
+     * Gibt Namen des Users zurück.
+     *
+     * @return Namen des Users.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gibt Schulden-Stand des Users zurück.
+     *
+     * @return Schulden-Stand des Users.
+     */
     public int getSchulden() {
         return schulden;
     }
 
+    /**
+     * Setzt den Schulden-Stand des Users.
+     *
+     * @param schulden neuer Schulden-Stand des Users.
+     */
     public void setSchulden(int schulden) {
         this.schulden = schulden;
     }
 
+    /**
+     * Gibt die Rolle des Users zurück.
+     *
+     * @return Rolle des Users.
+     */
     public String getRolle() {
         return rolle;
     }
 
+    /**
+     * Gibt den Pin des Users zurück.
+     *
+     * @return Pin des Users.
+     */
     public int getPin() {
         return this.pin;
     }
 
+    /**
+     * Gibt den Namen des Users zurück.
+     *
+     * @return Namen des Users.
+     */
     public String toString() {
         return this.getName();
     }
 
+    /**
+     * Gibt eine aufbereitete Representation der Schulden/Guthaben zurück.
+     *
+     * @return Aufbereitete Representation der Schulden/Guthaben
+     */
     public String getSchuldenString() {
         String schuldenValue = Integer.toString(this.schulden >= 0 ? this.schulden : this.schulden * -1);
         String schuldenType = this.schulden >= 0 ? "Schulden" : "Guthaben";
