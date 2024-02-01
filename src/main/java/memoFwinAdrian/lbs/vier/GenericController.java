fbf3cc5e6492abd39b073b53d56805a3db364631
fwin-lb4 <florian.windisch@lbs4.salzburg.at>
2024-02-01 10:30:21 +0100

    Generic-Controller JavaDoc ergänzen

--- src/main/java/memoFwinAdrian/lbs/vier/GenericController.java
+++ src/main/java/memoFwinAdrian/lbs/vier/GenericController.java
@@ -5,32 +5,60 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.util.List;
+
 /**
  * Generischer Controller, der gemeinsame Funktionalitäten für verschiedene Ansichten bereitstellt.
  */
 public class GenericController {
+    /**
+     * Die Stage der Applikation.
+     */
     protected Stage stage;
+    /**
+     * Die gemeinsame Scene.
+     */
     protected Scene scene;
+    /**
+     * Der vorherige Root.
+     */
     protected Parent previousRoot;
+    /**
+     * Der nachfolgende Root.
+     */
     protected GenericController previousController;
+    /**
+     * Der nachfolgende Root.
+     */
     protected Parent nextRoot;
+    /**
+     * Der nachfolgende Controller.
+     */
     protected GenericController nextController;
+    /**
+     * Datenbank-Zugang.
+     */
     protected Datenbank db;
+    /**
+     * Der UserHolder.
+     */
     protected UserHolder currentUser;
+    /**
+     * Liste der User.
+     */
     protected List<User> userList;
 
     /**
      * Initialisiert den Controller mit den erforderlichen Ressourcen.
      *
-     * @param db                  Datenbank-Objekt
-     * @param stage               JavaFX Stage-Objekt
-     * @param scene               JavaFX Scene-Objekt
-     * @param previousRoot        Vorherige Wurzel (Parent) der Szene
-     * @param previousController  Vorheriger Controller
-     * @param nextRoot            Nächste Wurzel (Parent) der Szene
-     * @param nextController      Nächster Controller
-     * @param currentUser         Benutzerinformationen
-     * @param userList            Liste der Benutzer
+     * @param db                 Datenbank-Objekt
+     * @param stage              JavaFX Stage-Objekt
+     * @param scene              JavaFX Scene-Objekt
+     * @param previousRoot       Vorherige Wurzel (Parent) der Szene
+     * @param previousController Vorheriger Controller
+     * @param nextRoot           Nächste Wurzel (Parent) der Szene
+     * @param nextController     Nächster Controller
+     * @param currentUser        Benutzerinformationen
+     * @param userList           Liste der Benutzer
      */
     public void setup(
             Datenbank db,
@@ -55,15 +83,18 @@ public class GenericController {
 
         this.init();
     }
+
     /**
      * Methode zur Initialisierung des Controllers.
      * Diese Methode kann von abgeleiteten Klassen überschrieben werden, um spezifische Initialisierungen vorzunehmen.
      */
-    protected void init() {}
+    protected void init() {
+    }
 
     /**
      * Methode zum Aktualisieren der Benutzeroberfläche.
      * Diese Methode kann von abgeleiteten Klassen überschrieben werden, um spezifische Aktualisierungen vorzunehmen.
      */
-    protected void refresh() {}
+    protected void refresh() {
+    }
 }
